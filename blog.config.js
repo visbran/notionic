529185f92ebb6a373e732827f2da380664f30849
Brandon Visca <brandon@Pixou-Air.lan>
2023-04-22 23:12:59 +0200

    update and fix auto

--- blog.config.js
+++ blog.config.js
@@ -1,18 +1,18 @@
 const BLOG = {
-  title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
-  link: 'https://zuolan.me',
-  newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  title: 'tl:dr - encore un blog informatique',
+  author: 'visbran',
+  email: 'contact@visbran.fr',
+  link: 'https://blog.visbran.fr',
+  newsletter: 'Newsletter de la semaine',
+  description: 'L’objectif principal de ce blog est d’en faire une sorte de bibliothèque de données ou je peux retrouver facilement, installation configuration & autre lié au domaine du web de l&informatique',
+  lang: 'fr-FR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  timezone: 'Europe/Paris', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#212936', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
   pagesShow: {
@@ -20,32 +20,32 @@ const BLOG = {
     notes: true,
     projects: true,
     contact: true,
-    books: true,
-    friends: true
+    books: false,
+    friends: false
   },
   showWeChatPay: true,
   previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateHost: 'og-zl.vercel.app/api/default?background=https%3A%2F%2Fi.ibb.co%2F2c8j3jm%2Fcool-background.png&border=solid&description=tl%3Adr&logo=https%3A%2F%2Fimg.icons8.com%2Fclouds%2F100%2Fnull%2Fcyber-security.png&siteName=blog.visbran.fr&summary=.&theme=light&title=.', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
   socialLink: {
-    twitter: 'https://twitter.com/izuolan',
-    github: 'https://github.com/izuolan',
-    telegram: 'https://t.me/zuolan'
+    twitter: 'https://twitter.com/visbran',
+    github: 'https://github.com/visbran',
+    telegram: 'https://t.me/visbran'
   },
   seo: {
-    keywords: ['Notionic', 'Zuolan', 'Blog'],
+    keywords: ['sysadmin', 'blog', 'blog administrateur système', 'tuto informatique', 'blog informatique geek', 'devops', 'linux'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionSpacesId: process.env.NOTION_SPACES_ID, // DO NOT CHANGE THIS! Edit .env file!
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  notionDomain: 'izuolan.notion.site',
+  notionDomain: 'visbran.notion.site',
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
-  telegramChatId: '263895784', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
-  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
+  telegramChatId: '', // The chat id of your Telegram bot
+  telegramChannelUrl: '', // The link of your Telegram channel
+  telegramChannelName: 'visbran', // The name of your Telegram channel
+  craftConfigShareUrl: '', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
     ackeeConfig: {
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'zh'],
-    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+    defaultLocale: 'fr',
     localeDetection: false
   },
   transpilePackages: ['dayjs'],
