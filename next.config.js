87f9d8418512150ee47a5bff82671f5fe5510750
Brandon Visca <brandon@Pixou-Air.lan>
2023-04-22 23:22:46 +0200

    update and fix auto

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
-    locales: ['fr'],
-    defaultLocale: 'fr',
+    locales: ['en'],
+    defaultLocale: 'en',
     localeDetection: false
   },
   transpilePackages: ['dayjs'],
